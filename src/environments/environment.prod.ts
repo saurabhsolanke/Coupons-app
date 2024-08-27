08746d5f7a9564000ee7c95837e5512a6a5ae7c8
Saurabh Solanke <35625425+saurabhsolanke@users.noreply.github.com>
2024-08-27 23:09:37 +0530

    SS | build error resolved

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 export const environment = {
-  production: true
-};
+    production: true,
+    firebaseConfig : {
+      apiKey: "AIzaSyAbDnqKtJc0bhu69ZCMHzjKLNJIXN_tPCs",
+      authDomain: "coupons-d3538.firebaseapp.com",
+      projectId: "coupons-d3538",
+      storageBucket: "coupons-d3538.appspot.com",
+      messagingSenderId: "417026396222",
+      appId: "1:417026396222:web:2b25745fc0ced43b70c16f",
+      measurementId: "G-N3R75LG2EG"
+    }
+  };
+
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyAbDnqKtJc0bhu69ZCMHzjKLNJIXN_tPCs",
+  //   authDomain: "coupons-d3538.firebaseapp.com",
+  //   projectId: "coupons-d3538",
+  //   storageBucket: "coupons-d3538.appspot.com",
+  //   messagingSenderId: "417026396222",
+  //   appId: "1:417026396222:web:2b25745fc0ced43b70c16f",
+  //   measurementId: "G-N3R75LG2EG"
+  // };
+
+  const app = initializeApp(environment.firebaseConfig);
+  const analytics = getAnalytics(app);
